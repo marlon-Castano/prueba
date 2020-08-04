d4aaa6d36be5b9e6e1a116b909628dee49590598
Marlon Castano <mscastanom@correo.udistrital.edu.co>
2020-08-03 21:57:47 -0500

    añadimos archivo HolaMundo.java con hola mundo y un saludo

---
+++ HolaMundo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ASUS
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "hola mundo");
+	hola que tal;
+    }
+    
+}
